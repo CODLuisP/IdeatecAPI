22f01f0ae3ca9dce96d0d056b994030dc4a43f3b
Diego272201 <diegocool36@gmail.com>
2026-02-19 10:38:01 -0500

    Cambios en unit of work

--- src/IdeatecAPI.Application/Common/Interfaces/Persistence/IUnitOfWork.cs
+++ src/IdeatecAPI.Application/Common/Interfaces/Persistence/IUnitOfWork.cs
@@ -3,24 +3,15 @@ namespace IdeatecAPI.Application.Common.Interfaces.Persistence;
 public interface IUnitOfWork : IDisposable
 {
     ICategoriaRepository Categorias { get; }
-<<<<<<< HEAD
     IUsuarioRepository Usuarios { get; }
+    IClienteRepository clientes {get; }
     IEmpresaRepository Empresas { get; }
-
-
-    // Agregar junto a ICategoriaRepository, IUsuarioRepository, IEmpresaRepository:
     INoteRepository Notes { get; }
     INoteDetailRepository NoteDetails { get; }
     INoteLegendRepository NoteLegends { get; }
-
-
-=======
-    IUsuarioRepository Usuarios { get; } 
-    IClienteRepository clientes {get; }
     
     // Transacciones
-    IEmpresaRepository Empresas { get; } 
->>>>>>> 4981bb4898f122372199cb065b3613abd4469e12
+
     void BeginTransaction();
     void Commit();
     void Rollback();
