f25b825c1bf168fbc2981a2f6d1659200f9fc089
Diego272201 <diegocool36@gmail.com>
2026-03-25 22:55:03 -0500

    Fcruador rol se ve solo

--- src/IdeatecAPI.Application/Common/Interfaces/Persistence/IUsuarioRepository.cs
+++ src/IdeatecAPI.Application/Common/Interfaces/Persistence/IUsuarioRepository.cs
@@ -10,7 +10,7 @@ public interface IUsuarioRepository
 
     Task<int> CreateAsync(Usuario usuario);
     Task<Usuario?> GetByIdAsync(int id);
-    Task<IEnumerable<Usuario>> GetAllAsync(bool soloActivos = true, string? ruc = null, string? sucursalID = null);
+    Task<IEnumerable<Usuario>> GetAllAsync(bool soloActivos = true, string? ruc = null, string? sucursalID = null, int? usuarioId = null);
     Task<bool> UpdateAsync(Usuario usuario);
     Task<bool> DeleteAsync(int id); // Soft delete (cambiar estado a false)
     Task<bool> ExistsAsync(string username, string? email = null, string? ruc = null, int? excludeId = null);
