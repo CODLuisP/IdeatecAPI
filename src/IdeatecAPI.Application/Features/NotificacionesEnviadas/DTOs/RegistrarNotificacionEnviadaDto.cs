5bf9c8b8b3884306a11a388c64b3662b9f933a8d
Diego272201 <diegocool36@gmail.com>
2026-06-03 23:36:23 -0500

    CAMBIOS EN Notf enviadas

--- src/IdeatecAPI.Application/Features/NotificacionesEnviadas/DTOs/RegistrarNotificacionEnviadaDto.cs
+++ src/IdeatecAPI.Application/Features/NotificacionesEnviadas/DTOs/RegistrarNotificacionEnviadaDto.cs
@@ -2,13 +2,7 @@ namespace IdeatecAPI.Application.Features.NotificacionesEnviadas.DTOs;
 
 public class RegistrarNotificacionEnviadaDto
 {
-    public string? NumDoc { get; set; }
-    public string? PeriodoTipo { get; set; }
-    public string? Moneda { get; set; }
-    public string? TipoDoc { get; set; }
-    public bool? EmailEnviado { get; set; }
-    public bool? WhatsappEnviado { get; set; }
-    public DateTime? FechaFin { get; set; }
-    public DateTime? FechaEnvio { get; set; }
-    public int? UsuarioId { get; set; }
+    public int Id { get; set; }
+    public bool EmailEnviado { get; set; }
+    public bool WhatsappEnviado { get; set; }
 }
